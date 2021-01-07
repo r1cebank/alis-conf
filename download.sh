62e34df9b6c7fff4ec084d289aa3ffd1e46643f7
Siyuan Gao <siyuangao@protonmail.com>
2021-01-06 17:01:13 -0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -8,9 +8,9 @@ rm -f alis_custom.conf
 rm -f alis_package_custom.sh
 rm -f configure.sh
 
-wget https://raw.githubusercontent.com/r1cebank/alis-conf/master/alis_custom.conf
-wget https://raw.githubusercontent.com/r1cebank/alis-conf/master/alis_package_custom.sh
-wget https://raw.githubusercontent.com/r1cebank/alis-conf/master/configure.sh
+wget https://raw.githubusercontent.com/r1cebank/alis-conf/main/alis_custom.conf
+wget https://raw.githubusercontent.com/r1cebank/alis-conf/main/alis_package_custom.sh
+wget https://raw.githubusercontent.com/r1cebank/alis-conf/main/configure.sh
 
 chmod +x alis_custom.sh
 chmod +x alis-alis_package_custom.sh
